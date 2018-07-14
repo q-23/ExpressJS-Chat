565a14ae53392a8531fab9f2020834e1826703e1
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-07-14 20:58:00 +0200

    Update app.js element, add userform

--- client/App.js
+++ client/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import styles from './App.css';
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import UsersList from './UsersList';
+import UserForm from './UserForm';
 
 const socket = io('/');
 
@@ -8,4 +13,70 @@ class App extends Component {
     super(props);
     this.state = {users: [], messages: [], text: '', name: ''};
   }
+
+	render() {
+	    return this.state.name !== '' ? (
+	      this.renderLayout()
+	    ) : this.renderUserForm() 
+	  }
+
+	componentDidMount() {
+	  socket.on('message', message => this.messageReceive(message));
+	  socket.on('update', ({users}) => this.chatUpdate(users));
+	}
+
+	messageReceive(message) {
+	  const messages = [message, ...this.state.messages];
+	  this.setState({messages});
+	}
+
+	chatUpdate(users) {
+	  this.setState({users});
+	}
+
+	handleMessageSubmit(message) {
+	  const messages = [message, ...this.state.messages];
+	  this.setState({messages});
+	  socket.emit('message', message);
+	}
+
+	handleUserSubmit(name) {
+	  this.setState({name});
+	  socket.emit('join', name);
+	}
+
+	renderLayout() {
+	   return (
+	      <div className={styles.App}>
+	        <div className={styles.AppHeader}>
+	          <div className={styles.AppTitle}>
+	            ChatApp
+	          </div>
+	          <div className={styles.AppRoom}>
+	            App room
+	          </div>
+	        </div>
+	        <div className={styles.AppBody}>
+	          <UsersList
+	            users={this.state.users}
+	          />
+	          <div className={styles.MessageWrapper}>
+	            <MessageList
+	              messages={this.state.messages}
+	            />
+	            <MessageForm
+	              onMessageSubmit={message => this.handleMessageSubmit(message)}
+	              name={this.state.name}
+	            />
+	          </div>
+	        </div>
+	      </div>
+	   );
+	}
+
+	renderUserForm() {
+	   return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
+	}
+
 };
+
--- client/index.js
+++ client/index.js
@@ -21,3 +21,38 @@ if (module.hot) {
     render(NewApp)
   });
 }
+
+render() {
+    return this.state.name !== '' ? (
+      this.renderLayout()
+    ) : this.renderUserForm() // zaimplementowane w późniejszej części
+  }
+
+renderLayout() {
+   return (
+      <div className={styles.App}>
+        <div className={styles.AppHeader}>
+          <div className={styles.AppTitle}>
+            ChatApp
+          </div>
+          <div className={styles.AppRoom}>
+            App room
+          </div>
+        </div>
+        <div className={styles.AppBody}>
+          <UsersList
+            users={this.state.users}
+          />
+          <div className={styles.MessageWrapper}>
+            <MessageList
+              messages={this.state.messages}
+            />
+            <MessageForm
+              onMessageSubmit={message => this.handleMessageSubmit(message)}
+              name={this.state.name}
+            />
+          </div>
+        </div>
+      </div>
+   );
+}
